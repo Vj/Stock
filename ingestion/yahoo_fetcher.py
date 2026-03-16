18465e41b3da3fdeb1d8e1a93b91be9ffdf2da3e
Vj <46293901+Vj@users.noreply.github.com>
2026-03-16 19:27:42 +0530

    Add/update ingestion/yahoo_fetcher.py

---
+++ ingestion/yahoo_fetcher.py
@@ -0,0 +1,9 @@
+﻿import yfinance as yf
+import pandas as pd
+
+
+def fetch_price_history(symbol):
+    ticker = yf.Ticker(symbol)
+    df = ticker.history(period="5y")
+    df.reset_index(inplace=True)
+    return df
