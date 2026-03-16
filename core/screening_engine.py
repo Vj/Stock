0e3a6d30311da57cc1d16277f2191cc1d8743afa
Vj <46293901+Vj@users.noreply.github.com>
2026-03-16 19:27:38 +0530

    Add/update core/screening_engine.py

---
+++ core/screening_engine.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+
+
+class ScreeningEngine:
+    def __init__(self, df):
+        self.df = df
+
+    def apply_filters(self, rules):
+        filtered = self.df[
+            (self.df["pe"] < rules.get("pe", 25)) &
+            (self.df["roe"] > rules.get("roe", 15)) &
+            (self.df["roce"] > rules.get("roce", 15)) &
+            (self.df["debt_equity"] < rules.get("debt_equity", 0.5))
+        ]
+        return filtered
