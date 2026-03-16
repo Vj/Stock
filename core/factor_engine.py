d01ebe4717274fc3ee59b0267410d7866fa541dd
Vj <46293901+Vj@users.noreply.github.com>
2026-03-16 19:27:36 +0530

    Add/update core/factor_engine.py

---
+++ core/factor_engine.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+
+
+class FactorEngine:
+    def __init__(self, df):
+        self.df = df
+
+    def calculate_momentum(self):
+        self.df["dma50"] = self.df["Close"].rolling(50).mean()
+        self.df["dma200"] = self.df["Close"].rolling(200).mean()
+        return self.df
+
+    def calculate_returns(self):
+        self.df["1y_return"] = self.df["Close"].pct_change(252)
+        return self.df
