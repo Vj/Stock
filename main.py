23e51da0bf1c3a9106300b0d96177e0a39702cfe
Vj <46293901+Vj@users.noreply.github.com>
2026-03-16 19:26:48 +0530

    Add/update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿from ingestion.yahoo_fetcher import fetch_price_history
+from core.factor_engine import FactorEngine
+
+
+def run():
+    symbol = "RELIANCE.NS"
+    df = fetch_price_history(symbol)
+    factor = FactorEngine(df)
+    df = factor.calculate_momentum()
+    print(df.tail())
+
+
+if __name__ == "__main__":
+    run()
