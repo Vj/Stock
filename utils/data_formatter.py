0b8d3bc979a758cade326788d5afcbd2bf406fc5
Vj <46293901+Vj@users.noreply.github.com>
2026-03-16 19:27:51 +0530

    Add/update utils/data_formatter.py

---
+++ utils/data_formatter.py
@@ -0,0 +1,10 @@
+﻿def format_stock_table(df):
+    columns = [
+        "symbol",
+        "score",
+        "roe",
+        "roce",
+        "pe",
+        "revenue_growth",
+    ]
+    return df[columns].sort_values(by="score", ascending=False)
